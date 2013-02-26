62ead017b1e55f01e11cdf11cd6e55db98b09aec
Mahesh <mahesh430023@gmail.com>
2013-02-27 02:48:52 +0530

    graph changes

--- src/demo/demograph.clj
+++ src/demo/demograph.clj
@@ -1,14 +1,68 @@
 (ns demo.demograph)
 
+; define table weighted directed graph
+(def 
+  wdg
+  '((2 (1 a)) (3 (1 g) (4 b)) (4 (5 c)) (6 (4 d e)) (7 (1 f))))
+
 ; define table directed graph
 (def 
-  tablegraph 
-  '((2 1) (7 1) (3 1 4) (4 5) (6 4))) ;{node neighbors) (2 1)
+  dg 
+  '((2 1) (3 1 4) (4 5) (6 4) (7 1))) ;{node neighbors) (2 1)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(defn
+  if-nil-or-empty
+  [any]
+  (or 
+    (nil? any) 
+    (empty? any)))
+  
+(defn
+  getrest2 
+  [restlst simplerestlst]
+  (if (if-nil-or-empty restlst)
+    (reverse simplerestlst)
+    (getrest2 
+      (rest restlst)
+      (cons 
+        (ffirst restlst) 
+        simplerestlst))))
+
+(defn
+  getrest
+  [restlst]
+  (getrest2 restlst nil))
+
+(defn 
+  get-simple-list
+  [list]
+  (cons 
+    (first list) 
+    (getrest (rest list))))
+
+(defn
+  dg-to-wdg
+  [wdg dg]
+  (if (if-nil-or-empty wdg)
+    dg
+    (dg-to-wdg 
+      (rest wdg)
+      (cons 
+        (get-simple-list (first wdg))
+        dg))))
+
+(defn
+  get-dg-from-wdg
+  "get directed graph from weighted directed graph"
+  [wdg]
+  (reverse (dg-to-wdg wdg nil)))
 
 (defn
   our-assoc
   [node net]
-  (if (or (nil? net) (empty? net)) 
+  (if (if-nil-or-empty net) 
     nil
     (let [pair (first net)]
       (if (== node (first pair))
@@ -26,7 +80,7 @@
   bfs
   [end queue net]
   (println "=> Q: " queue)
-  (if (or (nil? queue) (empty? queue)) 
+  (if (if-nil-or-empty queue) 
     nil
     (let [path (first queue)]
       (println "=> path: " path)
@@ -41,7 +95,7 @@
 
 (defn 
   shortest-path  
-  "Breadth first search algorithm"
+  "Breadth first search algorithm on dg"
   [start end net]
   (bfs end (list (list start)) net))
   
--- src/demo/test.clj
+++ src/demo/test.clj
@@ -1,2 +1,26 @@
 (ns demo.test)
 
+
+(def k '(3 (1 g) (4 b)))
+
+
+(defn
+  getrest2 
+  [restlst simplerestlst]
+  (if (or (nil? restlst) (empty? restlst))
+    (reverse simplerestlst)
+    (getrest2 
+      (rest restlst)
+      (cons 
+        (ffirst restlst) 
+        simplerestlst))))
+
+(defn
+  getrest
+  [restlst]
+  (getrest2 restlst nil))
+
+(defn 
+  get-simple-list
+  [list]
+  (cons (first list) (getrest (rest list))))
